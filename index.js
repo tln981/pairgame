059d8d5d0eee6e7f7badef89b18dd00279158946
tln981 <tln9815571@gmail.com>
2023-11-23 17:48:02 +0700

    fix bug load hinh

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { GameController } from "./Gamemanager.js";
 let fileImages=['./assets/0.jpg','./assets/1.jpg','./assets/2.jpg','./assets/3.jpg','./assets/4.jpg','./assets/5.jpg','./assets/6.jpg','./assets/7.jpg','./assets/8.jpg','./assets/9.jpg','./assets/cover.jpg','./assets/BackGround.jpg',]
-const preloadedImages = preloadImages(fileImages);
+
 
 function preloadImages(urls) {
     const images = [];
@@ -11,6 +11,8 @@ function preloadImages(urls) {
     }
     return images;
 }
-
 let game=new GameController();
+const preloadedImages = preloadImages(fileImages,() => {
+    game.createCard();
+  });
 game.createCard();
