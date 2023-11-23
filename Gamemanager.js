d731fbeb231943d2519aab9e1347877cd012c34f
tln981 <tln9815571@gmail.com>
2023-11-23 10:17:31 +0700

    fix bug load image

--- Gamemanager.js
+++ Gamemanager.js
@@ -3,6 +3,11 @@ import { Sprite } from "./Sprite.js";
 import { Label } from "./Label.js";
 import { Tween } from "./Tween.js";
 
+
+let fileImages=['./assets/0.jpg','./assets/1.jpg','./assets/2.jpg','./assets/3.jpg','./assets/4.jpg','./assets/5.jpg','./assets/6.jpg','./assets/7.jpg','./assets/8.jpg','./assets/9.jpg','./assets/cover.jpg','./assets/BackGround.jpg',]
+const preloadedImages = preloadImages(fileImages);
+
+
 let stackCard=[];
 let game = new Node();
 let animationController=new Tween()
@@ -16,7 +21,7 @@ point.positionX=1050;
 point.positionY=5;
 game.addChildLabel(point);
 game.draw();
- animationController.distributeCard(game.childrenSprite);
+animationController.distributeCard(game.childrenSprite);
 
 
 function openCard() {
@@ -33,9 +38,9 @@ function openCard() {
             pointValue-=500;
             if(pointValue<=0){
                 point.visibility='hidden';
-                let message = new Label('Bạn đã thua', 50, "red");
-                message.positionX = 100;
-                message.positionY = 300;
+                let message=new Label('Bạn đã thua thu!!!',50,"red");
+                message.positionX=100;
+                message.positionY=300;
                 message.draw()
             }
         } else {
@@ -73,3 +78,13 @@ function createCard(game) {
         game.addChildSprite(card);
     }
 }
+
+function preloadImages(urls) {
+    const images = [];
+    for (let i = 0; i < urls.length; i++) {
+      const img = new Image();
+      img.src = urls[i];
+      images.push(img);
+    }
+    return images;
+  }
