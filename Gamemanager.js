94349f5c5c154f713598bd06eba10d3dbab268b0
tln981 <tln9815571@gmail.com>
2023-11-23 16:01:54 +0700

    fix bug mo 1 luc nhieu card

--- Gamemanager.js
+++ Gamemanager.js
@@ -3,13 +3,14 @@ import { Sprite } from "./Sprite.js";
 import { Label } from "./Label.js";
 import { Tween } from "./Tween.js";
 
-export class GameController{
-    constructor(){
-        this.stackCard=[];
+export class GameController {
+    constructor() {
+        this.stackCard = [];
         this.game = new Node();
-        this.animationController=new Tween()
-        this.countResult=0;
-        this.pointValue=10000;
+        this.animationController = new Tween()
+        this.countResult = 0;
+        this.pointValue = 10000;
+        this.isStart = false;
     }
 
 
@@ -23,61 +24,73 @@ export class GameController{
             card.srcResult = "./assets/" + Math.floor(indexCard / 2) + ".jpg";
             this.game.addChildSprite(card);
         }
-        this.game.childrenSprite.forEach((card)=>card.element.addEventListener('click',(event)=>this.openCard(event)));
+        this.game.childrenSprite.forEach((card) => card.element.addEventListener('click', (event) => this.openCard(event)));
         //this.game.shuffleCard();
-        let point=new Label("$"+this.pointValue);
-        point.positionX=1050;
-        point.positionY=5;
+        let point = new Label("$" + this.pointValue);
+        point.positionX = 1050;
+        point.positionY = 5;
         this.game.addChildLabel(point);
         this.game.draw();
-        this.animationController.distributeCard(this.game.childrenSprite);
-        console.log(this.game);
+        this.animationController.distributeCard(this.game.childrenSprite,this.isStart);
+        setTimeout(() => {
+            this.isStart = true;
+        }, 4200)
     }
 
     openCard(event) {
-        console.log(this.game)
-        let currentCard = this.game.childrenSprite.find((card) => card.element == event.currentTarget);
-        this.animationController.flip(currentCard);
-        if (this.stackCard.length == 0) {
-            this.stackCard.push(currentCard);
-        } else {
-            let previousCard = this.game.childrenSprite.find((card) => card == this.stackCard[0]);
-            if (previousCard.srcResult != currentCard.srcResult) {
-                setTimeout(() => {
-                    this.animationController.flipOff(currentCard,previousCard);
-                }, 500)
-                this.pointValue-=500;
-                if(this.pointValue<=0){
-                    this.point.visibility='hidden';
-                    let message=new Label('Bạn đã thua thua!!!',50,"red");
-                    message.positionX=100;
-                    message.positionY=300;
-                    message.draw()
-                }
+        if (this.isStart == true) {
+            let currentCard = this.game.childrenSprite.find((card) => card.element == event.currentTarget);
+            this.animationController.flip(currentCard);
+            if (this.stackCard.length == 0) {
+                this.stackCard.push(currentCard);
             } else {
-                if (currentCard === previousCard) {
-                    this.animationController.flipOff(currentCard,previousCard);
-                    this.pointValue-=500;
-                } else {
+                this.isStart=false;
+                let previousCard = this.game.childrenSprite.find((card) => card == this.stackCard[0]);
+                if (previousCard.srcResult != currentCard.srcResult) {
                     setTimeout(() => {
-                        this.animationController.zoomOut(currentCard,previousCard);
+                        this.animationController.flipOff(currentCard, previousCard);
                     }, 500)
-                    this.countResult++;
-                    this.pointValue+=1000;
+                    setTimeout(() => {
+                        this.isStart=true;
+                    }, 2000)
+                    this.pointValue -= 500;
+                    if (this.pointValue <= 0) {
+                        this.point.visibility = 'hidden';
+                        let message = new Label('Bạn đã thua thua!!!', 50, "red");
+                        message.positionX = 100;
+                        message.positionY = 300;
+                        message.draw()
+                    }
+                } else {
+                    if (currentCard === previousCard) {
+                        this.animationController.flipOff(currentCard, previousCard);
+                        setTimeout(() => {
+                            this.isStart=true;
+                        }, 2000)
+                        this.pointValue -= 500;
+                    } else {
+                        setTimeout(() => {
+                            this.animationController.zoomOut(currentCard, previousCard);
+                        }, 500)
+                        setTimeout(() => {
+                            this.isStart=true;
+                        }, 2000)
+                        this.countResult++;
+                        this.pointValue += 1000;
+                    }
                 }
+                this.stackCard.pop();
             }
-            this.stackCard.pop();
-        }
-        this.game.childrenLabel[0].text="$"+this.pointValue;
-        if(this.countResult==10){
-            point.visibility='hidden';
-            let message=new Label('Bạn đã hoàn thành trò chơi với số điểm '+this.pointValue,50,"red");
-            message.positionX=100;
-            message.positionY=300;
-            message.draw()
+            this.game.childrenLabel[0].text = "$" + this.pointValue;
+            if (this.countResult == 10) {
+                point.visibility = 'hidden';
+                let message = new Label('Bạn đã hoàn thành trò chơi với số điểm ' + this.pointValue, 50, "red");
+                message.positionX = 100;
+                message.positionY = 300;
+                message.draw()
+            }
         }
     }
-    
 }
 
 
--- Tween.js
+++ Tween.js
@@ -20,18 +20,17 @@ export class Tween {
         object2.element.style.zIndex = "999";
         gsap.to(object1.element, { scale: 2, opacity: 0, duration, delay: duration * 2, onComplete: () => object1.visibility = 'hidden' });
         gsap.to(object2.element, { scale: 2, opacity: 0, duration, delay: duration * 2, onComplete: () => object2.visibility = 'hidden' });
-
     }
 
-    distributeCard(cards){
+    distributeCard(cards, isStart) {
         const duration = 0.2;
-        cards.forEach((card,index) => {
-            let tempX=card.positionX;
-            let tempY=card.positionY;
-            card.positionX=300;
-            card.positionY=300;
-            gsap.to(card, { positionX: tempX, positionY:tempY , duration, delay: duration * (index+1) });
-            gsap.to(card.element,{rotation :720, duration, delay: duration * (index+1) });
+        cards.forEach((card, index) => {
+            let tempX = card.positionX;
+            let tempY = card.positionY;
+            card.positionX = 300;
+            card.positionY = 300;
+            gsap.to(card, { positionX: tempX, positionY: tempY, duration, delay: duration * (index + 1) });
+            gsap.to(card.element, { rotation: 720, duration, delay: duration * (index + 1) });
         });
     }
 }
